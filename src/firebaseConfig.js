8f7b5e6697670fecae652961e05a77a514fab1e3
Mausom Saikia <uccmausom@gmail.com>
2020-05-27 11:32:55 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 import 'firebase/storage';
 const firebaseConfig = {
-    apiKey: "AIzaSyB8qlalrjfLOgQHZrZ2FGDODQoUG2uXHUQ",
-    authDomain: "storage-e5a5a.firebaseapp.com",
-    databaseURL: "https://storage-e5a5a.firebaseio.com",
-    projectId: "storage-e5a5a",
-    storageBucket: "storage-e5a5a.appspot.com",
-    messagingSenderId: "1092999515279",
-    appId: "1:1092999515279:web:2b55feec8df8cc199cf4fa",
-    measurementId: "G-71N1KBEM70"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 firebase.initializeApp(firebaseConfig);
